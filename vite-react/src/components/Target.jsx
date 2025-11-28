28a7b92f81018a4590a72342b0fdb857a84d6af6
Gopal chandrashekhar Gawas <146362802+Gopal562004@users.noreply.github.com>
2025-11-28 15:11:28 +0530

    Update Target.jsx

--- vite-react/src/components/Target.jsx
+++ vite-react/src/components/Target.jsx
@@ -1,18 +1,41 @@
+// import React, { useRef } from "react";
+// import { useGLTF, Float } from "@react-three/drei";
+
+// const Target = (props) => {
+//   const targetRef = useRef();
+//   const { scene } = useGLTF(
+//     "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf"
+//   );
+
+//   return (
+//     <Float
+//       speed={3} // a bit faster up/down oscillation
+//       rotationIntensity={1.2} // more visible subtle rotation
+//       floatIntensity={2.5} // more float movement
+//       floatingRange={[-0.6, 0.6]} // explicitly define vertical range
+//     >
+//       <primitive ref={targetRef} object={scene} {...props} />
+//     </Float>
+//   );
+// };
+
+// export default Target;
 import React, { useRef } from "react";
 import { useGLTF, Float } from "@react-three/drei";
 
 const Target = (props) => {
   const targetRef = useRef();
+
   const { scene } = useGLTF(
-    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf"
+    "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/WaterBottle/glTF/WaterBottle.gltf"
   );
 
   return (
     <Float
-      speed={3} // a bit faster up/down oscillation
-      rotationIntensity={1.2} // more visible subtle rotation
-      floatIntensity={2.5} // more float movement
-      floatingRange={[-0.6, 0.6]} // explicitly define vertical range
+      speed={3}
+      rotationIntensity={1.2}
+      floatIntensity={2.5}
+      floatingRange={[-0.6, 0.6]}
     >
       <primitive ref={targetRef} object={scene} {...props} />
     </Float>
@@ -20,3 +43,7 @@ const Target = (props) => {
 };
 
 export default Target;
+
+useGLTF.preload(
+  "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/WaterBottle/glTF/WaterBottle.gltf"
+);
