f380b1f21b61ab59856512c4742351ba73b6048c
Gopal chandrashekhar Gawas <146362802+Gopal562004@users.noreply.github.com>
2026-01-11 18:27:54 +0530

    Update main.jsx

--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -1,3 +1,15 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+// src/main.jsx
+import '../utils/preloadThree.js'; // Import this FIRST
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
